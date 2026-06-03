d3df90ab81e014401d5f1d1e4690da9865afc9d7
akash55warrior-beep <akash5.5warrior@gmail.com>
2026-06-03 21:12:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-console.log('BEASTMODEGYM website loaded');
+// Mobile Menu Toggle
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+
+// Navbar Shadow On Scroll
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(
+            this.getAttribute("href")
+        ).scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
+
+
+// Fade In Animation
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+document.querySelectorAll("section").forEach((section) => {
+
+    observer.observe(section);
+
+});
+
+
+// Hero Text Animation
+
+window.addEventListener("load", () => {
+
+    document.querySelector(".hero-content")
+    .classList.add("hero-loaded");
+
+});
+
+console.log("BEASTMODEGYM Loaded Successfully");
